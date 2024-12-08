01fe87c8f6d50a13350a02171c877058c15d55f2
Hera <mostafizhera@gmail.com>
2024-12-08 21:04:47 +0600

    added the server running vercel json file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
-require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 3000;
@@ -30,11 +30,18 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
    
     const movieCollection = client.db("movies").collection("movies");
     const favoriteCollection = client.db("movies").collection("favorite");
 
+
+    /// get defaults six  data functionality and showing data in UI---------
+
+    app.get('/six', async(req, res) => {
+      const sixMovie = await movieCollection.find().limit(6).sort( { ratting: -1 } ).toArray();
+      res.send(sixMovie);
+    })
 
     /// get data functionality and showing data in UI---------
 
@@ -121,7 +128,7 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
